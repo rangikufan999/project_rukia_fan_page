d27aba04cba3e4ec1373593018ee88f62d7beb71
rangikufan999 <rangikufan999@gmail.com>
2019-03-26 14:27:28 -0500

    Update rukia.js

--- scripts/rukia.js
+++ scripts/rukia.js
@@ -1,9 +1,11 @@
+//Used to change the banner in the top left corner of the page to a hover version of the image.
 function changeImage(element){
 	element.src = "img/corner-banner-hover.png";
 	element.style.marginLeft = "-24px";
 	element.style.marginTop = "-12px";
 }
 
+//Reverts the banner image in the corner back.
 function changeImageBack(element){
 	element.src = "img/corner-banner.png";
 	element.style.marginLeft = "-3px";
